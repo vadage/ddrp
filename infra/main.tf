eb96b77c6f53dbea6fc7926af1c7d51828dd068d
vadage <vadageofficial@gmail.com>
2025-08-03 18:42:36 +0200

    Rate limit remote function calls Configure basic bot management

--- infra/main.tf
+++ infra/main.tf
@@ -34,3 +34,32 @@ resource "cloudflare_workers_kv_namespace" "prod_messages_kv" {
   account_id = cloudflare_zone.prod_domain.account.id
   title      = "Prod ddrp messages"
 }
+
+resource "cloudflare_bot_management" "prod_bot_management" {
+  zone_id            = cloudflare_zone.prod_domain.id
+  ai_bots_protection = "block"
+  crawler_protection = "enabled"
+  fight_mode         = true
+}
+
+resource "cloudflare_ruleset" "prod_ratelimit" {
+  kind        = "zone"
+  name        = "Terraformed ratelimit"
+  phase       = "http_ratelimit"
+  zone_id     = cloudflare_zone.prod_domain.id
+  description = "Rate limit managed by terraform"
+  rules = [{
+    action      = "block"
+    description = "Remote function calls"
+    expression  = "(http.request.uri.path wildcard \"/_app/remote/*\")"
+    ratelimit = {
+      characteristics     = ["ip.src", "cf.colo.id"]
+      requests_to_origin  = false
+      rate_exceeds        = "request_base"
+      period              = 10
+      mitigation_timeout  = 10
+      requests_per_period = 5
+    }
+    customCounter = false
+  }]
+}
