ff4d65be770663c1f23e907adba48c817278c502
vadage <vadageofficial@gmail.com>
2025-11-01 15:37:34 +0100

    Move custom domain to wrangler config

--- infra/main.tf
+++ infra/main.tf
@@ -22,14 +22,6 @@ resource "cloudflare_zone" "prod_domain" {
   name = var.domain
 }
 
-resource "cloudflare_workers_custom_domain" "prod_worker_domain" {
-  account_id  = cloudflare_zone.prod_domain.account.id
-  environment = "production"
-  hostname    = cloudflare_zone.prod_domain.name
-  service     = "ddrp-production"
-  zone_id     = cloudflare_zone.prod_domain.id
-}
-
 resource "cloudflare_workers_kv_namespace" "prod_messages_kv" {
   account_id = cloudflare_zone.prod_domain.account.id
   title      = "Prod ddrp messages"
